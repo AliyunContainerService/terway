bd160e1cee1c84b62e12654b2dd93c28794007a0
l1b0k <libokang.dev@gmail.com>
2021-08-31 17:50:52 +0800

    fix get eni by mac may get incorrect nic in ipvlan mode
    Signed-off-by: l1b0k <libokang.dev@gmail.com>

--- pkg/link/interface.go
+++ pkg/link/interface.go
@@ -20,26 +20,15 @@ func GetDeviceNumber(mac string) (int32, error) {
 	}
 
 	for _, link := range linkList {
+		// ignore virtual nic type. eg. ipvlan veth bridge
+		if _, ok := link.(*netlink.Device); !ok {
+			continue
+		}
 		if link.Attrs().HardwareAddr.String() == mac {
 			return int32(link.Attrs().Index), nil
 		}
 	}
 	return 0, errors.Wrapf(ErrNotFound, "can't found dev by mac %s", mac)
-}
-
-// GetDeviceName get interface device name by mac address
-func GetDeviceName(mac string) (string, error) {
-	linkList, err := netlink.LinkList()
-	if err != nil {
-		return "", errors.Wrapf(err, "error get link list from netlink")
-	}
-
-	for _, link := range linkList {
-		if link.Attrs().HardwareAddr.String() == mac {
-			return link.Attrs().Name, nil
-		}
-	}
-	return "", errors.Wrapf(ErrNotFound, "can't found dev by mac %s", mac)
 }
 
 // DeleteIPRulesByIP delete all ip rule related to the addr
