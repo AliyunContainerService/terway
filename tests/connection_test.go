845762cde84aa270c5f27a8db6355b3fbaeaebcd
njucjc <njucjc@gmail.com>
2021-10-14 15:24:37 +0800

    fix e2e

--- tests/connection_test.go
+++ tests/connection_test.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/pkg/errors"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	corev1 "k8s.io/api/core/v1"
@@ -31,10 +33,9 @@ type ConnectionTestSuite struct {
 	RestConf               *rest.Config
 	ClientSet              kubernetes.Interface
 	PodNetworkingClientSet *versioned.Clientset
+	err                    error
 
 	TestCase []TestCase
-
-	TestNamespace string
 }
 
 func (s *ConnectionTestSuite) SetupSuite() {
@@ -43,66 +44,83 @@ func (s *ConnectionTestSuite) SetupSuite() {
 	s.ClientSet = utils.K8sClient
 	s.PodNetworkingClientSet, _ = versioned.NewForConfig(s.RestConf)
 
-	s.TestNamespace = "network-test"
-
 	ctx := context.Background()
+	s.T().Logf("test namespace: %s", testNamespace)
 	s.T().Logf("enable trunk: %v", enableTrunk)
 	s.T().Logf("enable policy: %v", enablePolicy)
-	s.T().Logf("creating namespace %s", s.TestNamespace)
-	err := EnsureNamespace(ctx, s.ClientSet, s.TestNamespace)
-	assert.NoError(s.T(), err)
+	s.T().Logf("creating namespace %s", testNamespace)
+	if s.err = EnsureNamespace(ctx, s.ClientSet, testNamespace); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to create namespace %s", testNamespace))
+	}
 
 	if enableTrunk {
 		s.T().Logf("creating %s pod networking", elasticPodNetWorking.Name)
-		_, err = EnsurePodNetworking(ctx, s.PodNetworkingClientSet, elasticPodNetWorking)
-		assert.NoError(s.T(), err)
+		if _, s.err = EnsurePodNetworking(ctx, s.PodNetworkingClientSet, elasticPodNetWorking); s.err != nil {
+			s.T().Error(errors.Wrapf(s.err, "fail to create pod networking %s", elasticPodNetWorking.Name))
+		}
 
 		s.T().Logf("creating %s pod networking", fixedPodNetWorking.Name)
-		_, err = EnsurePodNetworking(ctx, s.PodNetworkingClientSet, fixedPodNetWorking)
-		assert.NoError(s.T(), err)
+		if _, s.err = EnsurePodNetworking(ctx, s.PodNetworkingClientSet, fixedPodNetWorking); s.err != nil {
+			s.T().Error(errors.Wrapf(s.err, "fail to create pod networking %s", fixedPodNetWorking.Name))
+		}
 	}
 
 	s.T().Logf("creating %s", podConnA.Name)
-	_, err = EnsureDaemonSet(ctx, s.ClientSet, podConnA)
-	assert.NoError(s.T(), err)
+	if _, s.err = EnsureDaemonSet(ctx, s.ClientSet, podConnA); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to create pod %s", podConnA.Name))
+	}
 
 	s.T().Logf("creating %s", podConnB.Name)
-	_, err = EnsureDaemonSet(ctx, s.ClientSet, podConnB)
-	assert.NoError(s.T(), err)
+	if _, s.err = EnsureDaemonSet(ctx, s.ClientSet, podConnB); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to create pod %s", podConnB.Name))
+	}
 
 	s.T().Logf("creating %s", podConnC.Name)
-	_, err = EnsureDeployment(ctx, s.ClientSet, podConnC)
-	assert.NoError(s.T(), err)
+	if _, s.err = EnsureDeployment(ctx, s.ClientSet, podConnC); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to create pod %s", podConnC.Name))
+	}
 
 	s.T().Logf("creating %s", podConnD.Name)
-	_, err = EnsureStatefulSet(ctx, s.ClientSet, podConnD)
-	assert.NoError(s.T(), err)
+	if _, s.err = EnsureStatefulSet(ctx, s.ClientSet, podConnD); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to create pod %s", podConnD.Name))
+	}
+
+	s.T().Logf("creating %s", podConnPolicy.Name)
+	if _, s.err = EnsureDaemonSet(ctx, s.ClientSet, podConnPolicy); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to create network policy %s", podConnPolicy.Name))
+	}
 
 	s.T().Logf("creating %s for %s", clusterIPService.Name, podConnC.Name)
-	_, err = EnsureService(ctx, s.ClientSet, clusterIPService)
-	assert.NoError(s.T(), err)
+	if _, s.err = EnsureService(ctx, s.ClientSet, clusterIPService); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to create service %s", clusterIPService.Name))
+	}
 
 	s.T().Logf("creating %s for %s", nodePortService.Name, podConnC.Name)
-	_, err = EnsureService(ctx, s.ClientSet, nodePortService)
-	assert.NoError(s.T(), err)
+	if _, s.err = EnsureService(ctx, s.ClientSet, nodePortService); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to create service %s", nodePortService.Name))
+	}
 
 	s.T().Logf("creating %s for %s", loadBalancerService.Name, podConnC.Name)
-	_, err = EnsureService(ctx, s.ClientSet, loadBalancerService)
-	assert.NoError(s.T(), err)
+	if _, s.err = EnsureService(ctx, s.ClientSet, loadBalancerService); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to create service %s", loadBalancerService.Name))
+	}
 
 	s.T().Logf("creating %s for %s", headlessService.Name, podConnC.Name)
-	_, err = EnsureService(ctx, s.ClientSet, headlessService)
-	assert.NoError(s.T(), err)
+	if _, s.err = EnsureService(ctx, s.ClientSet, headlessService); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to create service %s", headlessService.Name))
+	}
 
 	if enablePolicy {
 		s.T().Logf("creating network policy %s", networkPolicy.Name)
-		_, err = EnsureNetworkPolicy(ctx, s.ClientSet, networkPolicy)
-		assert.NoError(s.T(), err)
+		if _, s.err = EnsureNetworkPolicy(ctx, s.ClientSet, networkPolicy); s.err != nil {
+			s.T().Error(errors.Wrapf(s.err, "fail to create network policy %s", networkPolicy.Name))
+		}
 	}
 
 	s.TestCase = []TestCase{
 		{
 			Type: TestTypePodToPod,
+			Skip: false,
 			Src: Resource{
 				Label: map[string]string{
 					"app": "container-network-pod-src",
@@ -119,6 +137,7 @@ func (s *ConnectionTestSuite) SetupSuite() {
 		},
 		{
 			Type: TestTypePodToPod,
+			Skip: enablePolicy,
 			Src: Resource{
 				Label: map[string]string{
 					"app": "host-network-pod-src",
@@ -131,10 +150,28 @@ func (s *ConnectionTestSuite) SetupSuite() {
 					"e2e": "true",
 				},
 			},
-			Status: !enablePolicy,
+			Status: true,
+		},
+		{
+			Type: TestTypePodToPod,
+			Skip: !enablePolicy,
+			Src: Resource{
+				Label: map[string]string{
+					"app": "container-network-policy-pod-src",
+					"e2e": "true",
+				},
+			},
+			Dst: Resource{
+				Label: map[string]string{
+					"app": "container-network-pod-dst",
+					"e2e": "true",
+				},
+			},
+			Status: false,
 		},
 		{
 			Type: TestTypePodToServiceIP,
+			Skip: false,
 			Src: Resource{
 				Label: map[string]string{
 					"app": "container-network-pod-src",
@@ -151,6 +188,7 @@ func (s *ConnectionTestSuite) SetupSuite() {
 		},
 		{
 			Type: TestTypePodToServiceIP,
+			Skip: enablePolicy,
 			Src: Resource{
 				Label: map[string]string{
 					"app": "host-network-pod-src",
@@ -163,10 +201,28 @@ func (s *ConnectionTestSuite) SetupSuite() {
 					"e2e": "true",
 				},
 			},
-			Status: !enablePolicy,
+			Status: true,
+		},
+		{
+			Type: TestTypePodToServiceIP,
+			Skip: !enablePolicy,
+			Src: Resource{
+				Label: map[string]string{
+					"app": "container-network-policy-pod-src",
+					"e2e": "true",
+				},
+			},
+			Dst: Resource{
+				Label: map[string]string{
+					"svc": "container-network-svc-dst",
+					"e2e": "true",
+				},
+			},
+			Status: false,
 		},
 		{
 			Type: TestTypePodToServiceName,
+			Skip: false,
 			Src: Resource{
 				Label: map[string]string{
 					"app": "container-network-pod-src",
@@ -183,9 +239,27 @@ func (s *ConnectionTestSuite) SetupSuite() {
 		},
 		{
 			Type: TestTypePodToServiceName,
+			Skip: enablePolicy,
 			Src: Resource{
 				Label: map[string]string{
 					"app": "host-network-pod-src",
+					"e2e": "true",
+				},
+			},
+			Dst: Resource{
+				Label: map[string]string{
+					"svc": "container-network-svc-dst",
+					"e2e": "true",
+				},
+			},
+			Status: false,
+		},
+		{
+			Type: TestTypePodToServiceName,
+			Skip: !enablePolicy,
+			Src: Resource{
+				Label: map[string]string{
+					"app": "container-network-policy-pod-src",
 					"e2e": "true",
 				},
 			},
@@ -201,79 +275,99 @@ func (s *ConnectionTestSuite) SetupSuite() {
 }
 
 func (s *ConnectionTestSuite) TearDownSuite() {
-	ctx := context.Background()
-	err := EnsureNamespace(ctx, s.ClientSet, s.TestNamespace)
-	assert.NoError(s.T(), err)
 
+	if s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "skip tear down resource in namespace %s, because of an error occurred.", testNamespace))
+		return
+	}
+	ctx := context.Background()
 	if enablePolicy {
 		s.T().Logf("delete %s", networkPolicy.Name)
-		err = DeleteNetworkPolicy(ctx, s.ClientSet, s.TestNamespace, networkPolicy.Name)
-		assert.NoError(s.T(), err)
+		if s.err = DeleteNetworkPolicy(ctx, s.ClientSet, testNamespace, networkPolicy.Name); s.err != nil {
+			s.T().Error(errors.Wrapf(s.err, "fail to delete network policy %s", networkPolicy.Name))
+		}
+	}
 
+	s.T().Logf("delete %s", clusterIPService.Name)
+	if s.err = DeleteService(ctx, s.ClientSet, testNamespace, clusterIPService.Name); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to delete service %s", clusterIPService.Name))
+	}
+
+	s.T().Logf("delete %s", nodePortService.Name)
+	if s.err = DeleteService(ctx, s.ClientSet, testNamespace, nodePortService.Name); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to delete service %s", nodePortService.Name))
+	}
+
+	s.T().Logf("delete %s", loadBalancerService.Name)
+	if s.err = DeleteService(ctx, s.ClientSet, testNamespace, loadBalancerService.Name); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to delete service %s", loadBalancerService.Name))
+	}
+
+	s.T().Logf("delete %s", headlessService.Name)
+	if s.err = DeleteService(ctx, s.ClientSet, testNamespace, headlessService.Name); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to delete service %s", headlessService.Name))
 	}
 
 	s.T().Logf("delete %s", podConnA.Name)
-	err = DeleteDaemonSet(ctx, s.ClientSet, s.TestNamespace, podConnA.Name)
-	assert.NoError(s.T(), err)
-
-	s.T().Logf("delete %s", clusterIPService.Name)
-	err = DeleteService(ctx, s.ClientSet, s.TestNamespace, clusterIPService.Name)
-	assert.NoError(s.T(), err)
-
-	s.T().Logf("delete %s", nodePortService.Name)
-	err = DeleteService(ctx, s.ClientSet, s.TestNamespace, nodePortService.Name)
-	assert.NoError(s.T(), err)
-
-	s.T().Logf("delete %s", loadBalancerService.Name)
-	err = DeleteService(ctx, s.ClientSet, s.TestNamespace, loadBalancerService.Name)
-	assert.NoError(s.T(), err)
-
-	s.T().Logf("delete %s", headlessService.Name)
-	err = DeleteService(ctx, s.ClientSet, s.TestNamespace, headlessService.Name)
-	assert.NoError(s.T(), err)
-
-	s.T().Logf("delete %s", podConnA.Name)
-	err = DeleteDaemonSet(ctx, s.ClientSet, s.TestNamespace, podConnA.Name)
-	assert.NoError(s.T(), err)
+	if s.err = DeleteDaemonSet(ctx, s.ClientSet, testNamespace, podConnA.Name); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to delete pod %s", podConnA.Name))
+	}
 
 	s.T().Logf("delete %s", podConnB.Name)
-	err = DeleteDaemonSet(ctx, s.ClientSet, s.TestNamespace, podConnB.Name)
-	assert.NoError(s.T(), err)
+	if s.err = DeleteDaemonSet(ctx, s.ClientSet, testNamespace, podConnB.Name); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to delete pod %s", podConnB.Name))
+	}
 
 	s.T().Logf("delete %s", podConnC.Name)
-	err = DeleteDeployment(ctx, s.ClientSet, s.TestNamespace, podConnC.Name)
-	assert.NoError(s.T(), err)
+	if s.err = DeleteDeployment(ctx, s.ClientSet, testNamespace, podConnC.Name); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to delete pod %s", podConnC.Name))
+	}
 
 	s.T().Logf("delete %s", podConnD.Name)
-	err = DeleteStatefulSet(ctx, s.ClientSet, s.TestNamespace, podConnD.Name)
-	assert.NoError(s.T(), err)
+	if s.err = DeleteStatefulSet(ctx, s.ClientSet, testNamespace, podConnD.Name); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to delete pod %s", podConnD.Name))
+	}
+
+	s.T().Logf("delete %s", podConnPolicy.Name)
+	if s.err = DeleteDaemonSet(ctx, s.ClientSet, testNamespace, podConnPolicy.Name); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to delete pod %s", podConnPolicy.Name))
+	}
 
 	if enableTrunk {
 		s.T().Logf("delete %s pod networking", elasticPodNetWorking.Name)
-		err = DeletePodNetworking(ctx, s.PodNetworkingClientSet, elasticPodNetWorking.Name)
-		assert.NoError(s.T(), err)
+		if s.err = DeletePodNetworking(ctx, s.PodNetworkingClientSet, elasticPodNetWorking.Name); s.err != nil {
+			s.T().Error(errors.Wrapf(s.err, "fail to delete pod networking %s", elasticPodNetWorking.Name))
+		}
 
 		s.T().Logf("delete %s pod networking", fixedPodNetWorking.Name)
-		err = DeletePodNetworking(ctx, s.PodNetworkingClientSet, fixedPodNetWorking.Name)
-		assert.NoError(s.T(), err)
+		if s.err = DeletePodNetworking(ctx, s.PodNetworkingClientSet, fixedPodNetWorking.Name); s.err != nil {
+			s.T().Error(errors.Wrapf(s.err, "fail to delete pod networking %s", fixedPodNetWorking.Name))
+		}
 	}
 
 	s.T().Logf("delete ns")
-	err = DeleteNamespace(ctx, s.ClientSet, s.TestNamespace)
-	assert.NoError(s.T(), err)
+	if s.err = DeleteNamespace(ctx, s.ClientSet, testNamespace); s.err != nil {
+		s.T().Error(errors.Wrapf(s.err, "fail to delete namespace %s", testNamespace))
+	}
 }
 
 func (s *ConnectionTestSuite) TestPod2Pod() {
 	for _, c := range s.TestCase {
-		if c.Type != TestTypePodToPod {
+		if c.Type != TestTypePodToPod || c.Skip {
 			continue
 		}
 		ctx := context.Background()
-		srcPods, err := ListPods(ctx, s.ClientSet, s.TestNamespace, c.Src.Label)
-		assert.NoError(s.T(), err)
+		var srcPods []corev1.Pod
+		srcPods, s.err = ListPods(ctx, s.ClientSet, testNamespace, c.Src.Label)
+		if s.err != nil {
+			s.T().Error(errors.Wrapf(s.err, "fail to list src pod %v", c.Src.Label))
+		}
 
-		dstPods, err := ListPods(ctx, s.ClientSet, s.TestNamespace, c.Dst.Label)
-		assert.NoError(s.T(), err)
+		var dstPods []corev1.Pod
+		dstPods, s.err = ListPods(ctx, s.ClientSet, testNamespace, c.Dst.Label)
+		if s.err != nil {
+			s.T().Error(errors.Wrapf(s.err, "fail to list dst pod %v", c.Dst.Label))
+		}
 		for _, src := range srcPods {
 			for _, dst := range dstPods {
 				s.T().Logf("src %s -> dst %s", podInfo(&src), podInfo(&dst))
@@ -290,33 +384,57 @@ func (s *ConnectionTestSuite) TestPod2Pod() {
 
 func (s *ConnectionTestSuite) TestPod2ServiceIP() {
 	for _, c := range s.TestCase {
-		if c.Type != TestTypePodToServiceIP {
+		if c.Type != TestTypePodToServiceIP || c.Skip {
 			continue
 		}
 		ctx := context.Background()
-		srcPods, err := ListPods(ctx, s.ClientSet, s.TestNamespace, c.Src.Label)
-		assert.NoError(s.T(), err)
+		var srcPods []corev1.Pod
+		srcPods, s.err = ListPods(ctx, s.ClientSet, testNamespace, c.Src.Label)
+		if s.err != nil {
+			s.T().Error(errors.Wrapf(s.err, "fail to list src pod %v", c.Src.Label))
+		}
 
-		dstServices, err := ListServices(ctx, s.ClientSet, s.TestNamespace, c.Dst.Label)
-		assert.NoError(s.T(), err)
+		var dstServices []corev1.Service
+		dstServices, s.err = ListServices(ctx, s.ClientSet, testNamespace, c.Dst.Label)
+		if s.err != nil {
+			s.T().Error(errors.Wrapf(s.err, "fail to list dst pod %v", c.Dst.Label))
+		}
 
-		nodeIPs, err := ListNodeIPs(context.Background(), s.ClientSet)
-		assert.NoError(s.T(), err)
+		var nodeIPs []net.IP
+		nodeIPs, s.err = ListNodeIPs(context.Background(), s.ClientSet)
+		if s.err != nil {
+			s.T().Error(errors.Wrap(s.err, "fail to list node ip"))
+		}
 
 		for _, src := range srcPods {
 			for _, svc := range dstServices {
 				var addrs []string
 				if svc.Spec.ClusterIP != corev1.ClusterIPNone {
+					if len(svc.Spec.Ports) < 1 {
+						s.err = errors.New("fail to allocate service port")
+						s.T().Error(s.err.Error())
+						continue
+					}
 					addrs = append(addrs, net.JoinHostPort(svc.Spec.ClusterIP, fmt.Sprintf("%d", svc.Spec.Ports[0].Port)))
 				}
 
 				if !enablePolicy {
 					if svc.Spec.Type == corev1.ServiceTypeLoadBalancer {
+						if len(svc.Status.LoadBalancer.Ingress) < 1 || len(svc.Spec.Ports) < 1 {
+							s.err = errors.New("fail to allocate load balancer ip or port")
+							s.T().Error(s.err.Error())
+							continue
+						}
 						addrs = append(addrs, net.JoinHostPort(svc.Status.LoadBalancer.Ingress[0].IP, fmt.Sprintf("%d", svc.Spec.Ports[0].Port)))
 					}
 
 					if svc.Spec.Type == corev1.ServiceTypeNodePort || svc.Spec.Type == corev1.ServiceTypeLoadBalancer {
 						for _, nodeIP := range nodeIPs {
+							if len(svc.Spec.Ports) < 1 {
+								s.err = errors.New("fail to allocate service node port")
+								s.T().Error(s.err.Error())
+								continue
+							}
 							addrs = append(addrs, net.JoinHostPort(nodeIP.String(), fmt.Sprintf("%d", svc.Spec.Ports[0].NodePort)))
 						}
 					}
@@ -334,15 +452,21 @@ func (s *ConnectionTestSuite) TestPod2ServiceIP() {
 
 func (s *ConnectionTestSuite) TestPod2ServiceName() {
 	for _, c := range s.TestCase {
-		if c.Type != TestTypePodToServiceName {
+		if c.Type != TestTypePodToServiceName || c.Skip {
 			continue
 		}
 		ctx := context.Background()
-		srcPods, err := ListPods(ctx, s.ClientSet, s.TestNamespace, c.Src.Label)
-		assert.NoError(s.T(), err)
+		var srcPods []corev1.Pod
+		srcPods, s.err = ListPods(ctx, s.ClientSet, testNamespace, c.Src.Label)
+		if s.err != nil {
+			s.T().Error(errors.Wrapf(s.err, "fail to list src pod %v", c.Src.Label))
+		}
 
-		dstServices, err := ListServices(ctx, s.ClientSet, s.TestNamespace, c.Dst.Label)
-		assert.NoError(s.T(), err)
+		var dstServices []corev1.Service
+		dstServices, s.err = ListServices(ctx, s.ClientSet, testNamespace, c.Dst.Label)
+		if s.err != nil {
+			s.T().Error(errors.Wrapf(s.err, "fail to list dst service %v", c.Dst.Label))
+		}
 
 		for _, src := range srcPods {
 			for _, svc := range dstServices {
@@ -359,15 +483,14 @@ func (s *ConnectionTestSuite) Expected(status bool, stdErrOut []byte, err error,
 		if assert.NoError(s.T(), err, msg) && assert.Equal(s.T(), 0, len(stdErrOut), msg) {
 			s.T().Logf(msg + ", test pass")
 		} else {
-			s.T().Errorf(msg + ", test failed")
-			s.T().Errorf("expected connection success, but connection failure")
+			s.err = err
+			s.T().Error(errors.Wrapf(err, "%s, test failed, expected connection success, but connection failure", msg))
 		}
 	} else {
 		if assert.Error(s.T(), err, msg) {
 			s.T().Logf(msg + ", test pass")
 		} else {
-			s.T().Errorf(msg + ", test failed")
-			s.T().Errorf("expected connection failure, but connection success")
+			s.T().Errorf(msg + ", test failed, expected connection failure, but connection success")
 		}
 	}
 }
