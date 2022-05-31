f13cc67f097d1ccff2e6d77db31240dcd209d10e
letty <letty.ll@alibaba-inc.com>
2022-05-31 15:57:41 +0800

    test: could schedule to node with arm taint

--- tests/prepare_test.go
+++ tests/prepare_test.go
@@ -181,6 +181,12 @@ func EnsureDeployment(ctx context.Context, cs kubernetes.Interface, cfg PodResCo
 							},
 						},
 					},
+					Tolerations: []corev1.Toleration{
+						{
+							Key: "kubernetes.io/arch",
+							Value: "arm64",
+						},
+					},
 				},
 			},
 		},
@@ -261,6 +267,12 @@ func EnsureStatefulSet(ctx context.Context, cs kubernetes.Interface, cfg PodResC
 							LabelSelector: &metav1.LabelSelector{
 								MatchLabels: cfg.Labels,
 							},
+						},
+					},
+					Tolerations: []corev1.Toleration{
+						{
+							Key: "kubernetes.io/arch",
+							Value: "arm64",
 						},
 					},
 				},
