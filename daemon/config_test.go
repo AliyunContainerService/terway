cf70d74c255e08378259c1edc1c8b975d99bcc1e
l1b0k <libokang.lbk@alibaba-inc.com>
2023-12-19 19:06:05 +0800

    pool: fix min size cal

---
+++ daemon/config_test.go
@@ -0,0 +1,67 @@
+package daemon
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/AliyunContainerService/terway/pkg/aliyun"
+	"github.com/AliyunContainerService/terway/types/daemon"
+)
+
+func init() {
+	aliyun.Test = true
+}
+
+func TestGetPoolConfigWithVPCMode(t *testing.T) {
+	cfg := &daemon.Config{
+		MaxPoolSize: 5,
+		MinPoolSize: 1,
+		EniCapRatio: 1,
+		RegionID:    "foo",
+	}
+	limit := &aliyun.Limits{
+		Adapters:           10,
+		MemberAdapterLimit: 5,
+	}
+	poolConfig, err := getPoolConfig(cfg, "VPC", limit)
+	assert.NoError(t, err)
+	assert.Equal(t, 5, poolConfig.MaxPoolSize)
+	assert.Equal(t, 1, poolConfig.MinPoolSize)
+}
+
+func TestGetPoolConfigWithENIOnlyMode(t *testing.T) {
+	cfg := &daemon.Config{
+		MaxPoolSize: 5,
+		MinPoolSize: 1,
+		EniCapRatio: 1,
+		RegionID:    "foo",
+	}
+	limit := &aliyun.Limits{
+		Adapters:           10,
+		MemberAdapterLimit: 5,
+	}
+	poolConfig, err := getPoolConfig(cfg, "ENIOnly", limit)
+	assert.NoError(t, err)
+	assert.Equal(t, 5, poolConfig.MaxPoolSize)
+	assert.Equal(t, 1, poolConfig.MinPoolSize)
+}
+
+func TestGetPoolConfigWithENIMultiIPMode(t *testing.T) {
+	cfg := &daemon.Config{
+		MaxPoolSize: 5,
+		MinPoolSize: 1,
+		EniCapRatio: 1,
+		RegionID:    "foo",
+	}
+	limit := &aliyun.Limits{
+		Adapters:           10,
+		IPv4PerAdapter:     5,
+		MemberAdapterLimit: 5,
+	}
+	poolConfig, err := getPoolConfig(cfg, "ENIMultiIP", limit)
+	assert.NoError(t, err)
+	assert.Equal(t, 5, poolConfig.MaxPoolSize)
+	assert.Equal(t, 1, poolConfig.MinPoolSize)
+	assert.Equal(t, 5, poolConfig.MaxIPPerENI)
+}
