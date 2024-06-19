9e4fb55fd8f7e2525a11dc578fb1de121c39e128
bingshen.wbs <bingshen.wbs@alibaba-inc.com>
2024-06-19 14:02:49 +0800

    support install driver via RunCommand
    Signed-off-by: bingshen.wbs <bingshen.wbs@alibaba-inc.com>

--- tests/erdma/setup_test.go
+++ tests/erdma/setup_test.go
@@ -10,9 +10,16 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
+	"github.com/samber/lo"
+	"github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/util/wait"
 
 	"go.uber.org/atomic"
 	corev1 "k8s.io/api/core/v1"
@@ -113,7 +120,15 @@ func TestMain(m *testing.M) {
 				skipTest = true
 				return ctx, nil
 			}
-			err = installDrivers(ctx, config)
+			err = installDriversByOOS(ctx, config)
+			if err != nil {
+				err1 := installDrivers(ctx, config)
+				if err1 != nil {
+					err = fmt.Errorf("error install driver: oos: %v, exec: %v", err, err1)
+				} else {
+					err = nil
+				}
+			}
 			if err != nil {
 				return nil, err
 			}
@@ -127,6 +142,88 @@ func TestMain(m *testing.M) {
 	)
 
 	os.Exit(testenv.Run(m))
+}
+
+func installDriversByOOS(ctx context.Context, config *envconf.Config) error {
+	accessKey := os.Getenv("ACCESS_KEY_ID")
+	accessKeySecret := os.Getenv("ACCESS_KEY_SECRET")
+	regionID := os.Getenv("REGION")
+	if accessKey == "" || accessKeySecret == "" || regionID == "" {
+		return fmt.Errorf("no available credential config: len(ak):%v, len(aksecret):%v, %v", len(accessKey), len(accessKeySecret), regionID)
+	}
+	client, err := ecs.NewClientWithAccessKey(regionID, accessKey, accessKeySecret)
+	if err != nil {
+		return fmt.Errorf("error create ecs client: %v", err)
+	}
+	nodeList := &corev1.NodeList{}
+	err = config.Client().Resources().List(ctx, nodeList)
+	if err != nil {
+		return fmt.Errorf("error list node: %v", err)
+	}
+	if len(nodeList.Items) == 0 {
+		return fmt.Errorf("no available node")
+	}
+
+	request := ecs.CreateRunCommandRequest()
+	request.Type = "RunShellScript"
+	request.CommandContent =
+		"curl -OL 'https://elastic-rdma.oss-cn-hangzhou.aliyuncs.com/wip/archive/erdma_installer_202311071611.tar.gz' &&  " +
+			"tar -xzvf erdma_installer_202311071611.tar.gz && cd erdma_installer && " +
+			"yum install -y gcc-c++ dkms cmake && ./install.sh --batch"
+	request.Name = "installErdmaDriver"
+	var instanceIDs []string
+	for _, node := range nodeList.Items {
+		providerID := strings.Split(node.Spec.ProviderID, ".")
+		if len(providerID) != 2 {
+			logrus.Warnf("error parse providerID: %v", node.Spec.ProviderID)
+			continue
+		}
+		instanceIDs = append(instanceIDs, providerID[1])
+	}
+	if len(instanceIDs) == 0 {
+		return fmt.Errorf("no available node")
+	}
+	if len(instanceIDs) > 100 {
+		instanceIDs = instanceIDs[:100]
+	}
+	request.InstanceId = &instanceIDs
+	request.Timeout = requests.NewInteger(600)
+	response, err := client.RunCommand(request)
+	if err != nil {
+		return fmt.Errorf("error run command: %v", err)
+	}
+	var innerErr error
+	err = wait.ExponentialBackoffWithContext(ctx, wait.Backoff{
+		Duration: 1 * time.Minute,
+		Steps:    10,
+	}, func(ctx context.Context) (bool, error) {
+		describeInvocationRequest := ecs.CreateDescribeInvocationsRequest()
+		describeInvocationRequest.InvokeId = response.InvokeId
+		resp, err := client.DescribeInvocations(describeInvocationRequest)
+		if err != nil {
+			return false, nil
+		}
+		done := true
+		lo.ForEach(resp.Invocations.Invocation, func(invocation ecs.Invocation, _ int) {
+			lo.ForEach(invocation.InvokeInstances.InvokeInstance, func(invocationInstance ecs.InvokeInstance, _ int) {
+				if invocationInstance.InstanceInvokeStatus != "Finished" {
+					done = false
+				} else {
+					if invocationInstance.ErrorCode != "0" {
+						innerErr = fmt.Errorf("error run command: %+v", invocationInstance)
+					}
+				}
+			})
+		})
+		if !done {
+			return false, nil
+		}
+		return true, nil
+	})
+	if err != nil {
+		return fmt.Errorf("error run command: %v, %v", err, innerErr)
+	}
+	return nil
 }
 
 func installDrivers(ctx context.Context, config *envconf.Config) error {
