369aff7e3c2476f900dc168ca25f43d3c4eba6c7
l1b0k <libokang.lbk@alibaba-inc.com>
2024-12-12 16:04:46 +0800

    add the deadline for the allocating requests
    Signed-off-by: l1b0k <libokang.lbk@alibaba-inc.com>

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -190,7 +190,7 @@ func (n *networkService) AllocIP(ctx context.Context, r *rpc.AllocIPRequest) (*r
 		if pod.PodENI {
 			resourceRequests = append(resourceRequests, &eni.RemoteIPRequest{})
 		} else {
-			req := &eni.LocalIPRequest{}
+			req := eni.NewLocalIPRequest()
 			if pod.ERdma {
 				req.LocalIPType = eni.LocalIPTypeERDMA
 			}
@@ -208,7 +208,7 @@ func (n *networkService) AllocIP(ctx context.Context, r *rpc.AllocIPRequest) (*r
 		if pod.PodENI || n.ipamType == types.IPAMTypeCRD {
 			resourceRequests = append(resourceRequests, &eni.RemoteIPRequest{})
 		} else {
-			req := &eni.LocalIPRequest{}
+			req := eni.NewLocalIPRequest()
 
 			if len(oldRes.GetResourceItemByType(daemon.ResourceTypeENI)) == 1 {
 				old := oldRes.GetResourceItemByType(daemon.ResourceTypeENI)[0]
