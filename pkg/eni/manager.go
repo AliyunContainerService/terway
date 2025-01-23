16efb6dac5b7c73d836e6878a0cf9187c888d7a0
l1b0k <libokang.lbk@alibaba-inc.com>
2025-01-23 20:01:12 +0800

    fix: potential deadlock
    Signed-off-by: l1b0k <libokang.lbk@alibaba-inc.com>

--- pkg/eni/manager.go
+++ pkg/eni/manager.go
@@ -207,7 +207,11 @@ func (m *Manager) Allocate(ctx context.Context, cni *daemon.CNI, req *AllocReque
 					cancel()
 					break
 				}
-				resultCh <- resp.NetworkConfigs
+
+				select {
+				case <-ctx.Done():
+				case resultCh <- resp.NetworkConfigs:
+				}
 			}
 		}()
 	}
