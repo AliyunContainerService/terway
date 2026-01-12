48d9f4263e201f8047a8d2a8cc8a89aef3b7212b
l1b0k <libokang.lbk@alibaba-inc.com>
2026-01-12 13:39:22 +0800

    feat: add datapath configuration and update version to v0.8.0
    Signed-off-by: l1b0k <libokang.lbk@alibaba-inc.com>

--- pkg/apis/crds/register.go
+++ pkg/apis/crds/register.go
@@ -58,7 +58,7 @@ func getCRD(name string) apiextensionsv1.CustomResourceDefinition {
 		version = "v0.1.0"
 	case CRDNode:
 		crdBytes = crdsNode
-		version = "v0.7.0"
+		version = "v0.8.0"
 	case CRDNodeRuntime:
 		crdBytes = crdsNodeRuntime
 		version = "v0.1.0"
